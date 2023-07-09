cc0221db284641b7605b6b6e330d8f8956297cfe
rahulpatil <rahul.p@healthcarenlp.com>
2023-07-09 21:26:59 +0530

    test check

--- const_prompts.py
+++ const_prompts.py
@@ -1,10 +1,10 @@
 
 # PROMPT_beautify_code = "please optimize given python code also please provide only code:\n"
-PROMPT_beautify_code ='Act as a expert code programmer who provides only code no explanation'
+PROMPT_beautify_code ='Act as a expert code programmer who provides only code no explanation '
 PROMPT_code_score = "Act as a expert code programmer who rates this code out of 100 "
-PROMPT_add_comments = "Act as a expert code programmer who adds meaningful comments to this code"
+PROMPT_add_comments = "Act as a expert code programmer who adds meaningful comments with the code, dont write only comments seperately "
 PROMPT_alternate_code = "Act as a expert code programmer who can give alternate version of this code "
-PROMPT_preprare_doc = "Act as a expert code programmer who documents this code"
+PROMPT_preprare_doc = "Act as a expert code programmer who documents this code for office purpose "
 PROMPT_space_time_complexity = "Act as a expert code programmer who can find time and space complexity of this code "
 PROMPT_test_cases = "Act as a expert code programmer who writes test cases for this code"
 PROMPT_code_snippet_gen = ""
