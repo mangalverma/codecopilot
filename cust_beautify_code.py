99f7776d8fdf07c9d409c8a5d0120ab300a8b40f
rahulpatil <rahul.p@healthcarenlp.com>
2023-07-09 19:07:34 +0530

    test check

--- cust_beautify_code.py
+++ cust_beautify_code.py
@@ -1,4 +1,4 @@
-
+import time
 import openai
 openai.api_key = "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"
 
@@ -6,25 +6,39 @@ from utils import split_based_on_lines
 from utils import prostprocess_code_results
 
 def cust_beautify_code(code_str, base_prompt):
-    list_clips = split_based_on_lines(code_str)
-    list_clips = [x for x in list_clips if len(x.strip())>0]
-    list_results = []
-    for c in list_clips:
-        response = openai.ChatCompletion.create(
+    #generate first results
+    response_first = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             messages=[
-                {"role": "system", "content": base_prompt + c}
+                {"role": "system", "content": base_prompt + code_str}
             ]
         )
-        #
-        if response['choices'][0]['finish_reason'] == "stop":
-            list_results.append(response.choices[0]['message']['content'])
-        else:
-            list_results.append(c)
+    if response_first['choices'][0]['finish_reason'] == "stop":
+        return response_first.choices[0]['message']['content']
 
-        # print("inide chatgpt")
+    else:
 
-    return "\n".join(list_results)
+        list_clips = split_based_on_lines(code_str)
+        list_clips = [x for x in list_clips if len(x.strip())>0]
+        list_results = []
+        for c in list_clips:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-16k",
+                messages=[
+                    {"role": "system", "content": base_prompt + c}
+                ]
+            )
+            #
+            if response['choices'][0]['finish_reason'] == "stop":
+                list_results.append(response.choices[0]['message']['content'])
+            else:
+                list_results.append(c)
+
+            time.sleep(1)
+
+            # print("inide chatgpt")
+
+        return "\n".join(list_results)
 
 def basic_call(code_str, base_prompt):
     if len(code_str)>0:
